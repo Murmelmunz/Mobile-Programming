95e828974f246b1527d06b9847dedb058bd237d4
ibdursun <i.dursun@outlook.de>
2019-05-10 18:48:15 +0200

    ws implemented

--- 02 Backend/channel.dart
+++ 02 Backend/channel.dart
@@ -1,29 +1,21 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:aqueduct/aqueduct.dart';
-import 'controller/TestController.dart';
 import 'controller/RoomController.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
 class Channel extends ApplicationChannel {
-  DbCollection testCollection;
   DbCollection roomCollection;
-  
+
+  var socket;
+
+  List<WebSocket> websockets = [];
+
   @override
   Future prepare() async {
     Db db = new Db("mongodb://localhost:27017/test");
     await db.open();
-    testCollection = db.collection('test');
     roomCollection = db.collection('rooms');
-    insert();
-  }
-
-  insert() async {
-    await this.testCollection.insertAll([
-      {'login': 'jdoe', 'name': 'John Doe', 'email': 'john@doe.com'},
-      {'login': 'lsmith', 'name': 'Lucy Smith', 'email': 'lucy@smith.com'},
-      {'login': 'a', 'name': 'a a', 'email': 'a@a.com'}
-    ]);
   }
 
   @override
@@ -34,9 +26,18 @@ class Channel extends ApplicationChannel {
       return new Response.ok('Hello world')..contentType = ContentType.TEXT;
     });
 
-    router.route("/test/[:id]").link(() => TestController(this.testCollection));
+    router.route("/room/[:id]").link(() => RoomController(this.roomCollection, this.socket));
 
-    router.route("/room/[:id]").link(() => RoomController(this.roomCollection));
+    router.route("/connect").linkFunction((request) async {
+      socket = await WebSocketTransformer.upgrade(request.raw);
+      socket.listen((a) => {
+        print(a)
+      });
+
+      socket.add("data");
+
+      return null;
+    });
 
     return router;
   }
---
+++ 02 Backend/client.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:aqueduct/aqueduct.dart';
+import 'controller/RoomController.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+
+void main() async {
+  var socket = await WebSocket.connect("ws://localhost:3000/connect");
+  socket.add("Test");
+  socket.listen((a) => {print(a)});
+}
